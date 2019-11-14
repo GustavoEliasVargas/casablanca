c20017db8d169cf1dd8a59625f5a60612eb14f72
GustavoEliasVargas <52014044+GustavoEliasVargas@users.noreply.github.com>
2019-11-14 12:52:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?PHP
 echo ('Hola mundo');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h4>Hola mundo</h4>
+</body>
+</html>
+
